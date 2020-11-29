8f554a65b57bcd4ce7faca62c8fcf7428b2bc227
Melissa Lee <melissalee@Melissas-MacBook-Pro-2.local>
2020-11-29 18:10:59 -0500

    updated the variants.R code and data files, minor changes to writing of some paragraphs

--- data/create_can_lang_variants.R
+++ data/create_can_lang_variants.R
@@ -5,8 +5,7 @@ library(RSQLite)
 
 devtools::install_github("ttimbers/canlang")
 library(canlang)
-
-write.csv(can_lang, "data/can_lang.csv")
+write_csv(can_lang, "can_lang.csv")
 
 main <- function(){
   path <- "can_lang.csv"
@@ -15,20 +14,18 @@ main <- function(){
   
   # file with some meta data at the top
   meta_data <- data.frame(metadata = c("Data source: https://ttimbers.github.io/canlang/",
-                                   "Data originally published in: Statistics Canada, Census of Population, 2016. Reproduced and distributed on an as is basis with the permission of Statistics Canada.",
-                                   "Date collected: 2020-11-29"))
+                                   "Data originally published in: Statistics Canada Census of Population 2016.",
+                                   "Reproduced and distributed on an as is basis with the permission of Statistics Canada."))
   write.table(meta_data, 
             file = paste0(path_no_prefix, "_meta-data.csv"),
             sep = ",",
             col.names = FALSE,
             row.names = FALSE,
             quote = FALSE)
-  write.table(data, 
-            file = paste0(path_no_prefix, "_meta-data.csv"),
-            sep = ",",
-            row.names = FALSE,
-            quote = FALSE,
-            append = TRUE)
+  write_csv(data, 
+            path = paste0(path_no_prefix, "_meta-data.csv"),
+            append = TRUE, 
+            col_names = TRUE)
   
   # file with no column names and tab delimiters
   write_delim(data, 
