f48b3686473ae5fc66cdf059a4549509d062c471
Melissa Lee <melissalee@Melissas-MacBook-Pro-2.local>
2020-12-03 17:55:39 -0500

    changed gather to pivot_longer up to section 3.4.3, added figure captions

--- data/create_untidy_canlang.R
+++ data/create_untidy_canlang.R
@@ -1,17 +1,19 @@
 ## creating untidy canlang data
 library(canlang)
 library(tidyverse)
+
+  
 top5_cities <- region_lang %>% 
   filter(region %in% c("Toronto", "Montreal", "Vancouver", "Calgary", "Edmonton"))
 
-top5_cities_wide <- top5_cities %>%
-  pivot_wider(names_from = region, values_from = c(mother_tongue, most_at_home, most_at_work, lang_known)
-)
+top5_cities_wide <- top5_cities %>% 
+  select(region, category, language, mother_tongue) %>% 
+  pivot_wider(names_from = region, values_from = c(mother_tongue))
 
-write.csv(top5_cities_wide, "region_lang_top5_cities_wide.csv")
+write.csv(top5_cities_wide, "region_lang_top5_cities_wide.csv", row.names = F)
 
 top5_cities_narrow <- top5_cities %>% 
   pivot_longer(!c(region, category, language), names_to = "type", values_to = "count")
 
-write.csv(top5_cities_narrow, "region_lang_top5_cities_narrow.csv")
+write.csv(top5_cities_narrow, "region_lang_top5_cities_narrow.csv", row.names = F)
 
