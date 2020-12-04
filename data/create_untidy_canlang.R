c31811da25a52fbe9867bc1a27d731c74fb21867
Melissa Lee <melissalee@Melissas-MacBook-Pro-2.local>
2020-12-04 13:24:15 -0500

    making messy dataset with delimiters, replacing example

--- data/create_untidy_canlang.R
+++ data/create_untidy_canlang.R
@@ -2,7 +2,7 @@
 library(canlang)
 library(tidyverse)
 
-View(region_lang)
+# making a wide data set 
 top5_cities <- region_lang %>% 
   filter(region %in% c("Toronto", "Montréal", "Vancouver", "Calgary", "Edmonton"))
 
@@ -13,19 +13,24 @@ top5_cities_wide <- top5_cities %>%
 
 write.csv(top5_cities_wide, "region_lang_top5_cities_wide.csv", row.names = F)
 
-
-library(cancensus)
-top5_cities_united <- top5_cities %>%   
-  select(region, category, language, mother_tongue) %>% 
-  unite("language", category, language, sep = "/")
-
-View(top5_cities_united)
-
-
-write.csv(top5_cities_united, "region_lang_top5_cities_messy.csv", row.names = F)
-
+# making a narrow data set 
 top5_cities_narrow <- top5_cities %>% 
   pivot_longer(!c(region, category, language), names_to = "type", values_to = "count")
 
 write.csv(top5_cities_narrow, "region_lang_top5_cities_narrow.csv", row.names = F)
 
+
+## making a messy data set with "/" delimiters 
+region_joined <- left_join(region_lang, region_data, by = "region")
+
+top5_cities_pop <- region_joined %>% 
+  filter(region %in% c("Toronto", "Montréal", "Vancouver", "Calgary", "Edmonton"))
+
+
+top5_cities_messy <- top5_cities_pop %>%   
+  select(region, category, language, mother_tongue, population) %>% 
+  unite("value", mother_tongue, population, sep = "/") %>% 
+  pivot_wider(names_from = region, values_from = c(value)) %>% 
+  select(category, language, Toronto, Montréal, Vancouver, Calgary, Edmonton)
+
+write.csv(top5_cities_messy, "region_lang_top5_cities_messy.csv", row.names = F)
