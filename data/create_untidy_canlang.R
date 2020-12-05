2d924f7f4e79b6a8ceaf7b0924fcf60bbf852b5f
Melissa Lee <melissalee@Melissas-MacBook-Pro-2.local>
2020-12-04 20:33:55 -0500

    added section about long to wide, updated last few sections about pipe operator and summary stats

--- data/create_untidy_canlang.R
+++ data/create_untidy_canlang.R
@@ -2,11 +2,13 @@
 library(canlang)
 library(tidyverse)
 
-
-# making a wide data set 
+# subsetting 5 cities
 top5_cities <- region_lang %>% 
   filter(region %in% c("Toronto", "Montréal", "Vancouver", "Calgary", "Edmonton"))
 
+write.csv(top5_cities, "region_lang_top5_cities.csv", row.names = F)
+
+# making a wide data set 
 top5_cities_wide <- top5_cities %>% 
   select(region, category, language, mother_tongue) %>% 
   pivot_wider(names_from = region, values_from = c(mother_tongue)) %>% 
@@ -24,26 +26,8 @@ top5_cities_messy <- top5_cities %>%
 write.csv(top5_cities_messy, "region_lang_top5_cities_messy.csv", row.names = F)
 
 
-
-
-
-region_joined <- left_join(region_lang, region_data, by = "region")
-
-top5_cities_pop <- region_joined %>% 
-  filter(region %in% c("Toronto", "Montréal", "Vancouver", "Calgary", "Edmonton"))
-
-
-top5_cities_messy <- top5_cities_pop %>%   
-  select(region, category, language, mother_tongue, population) %>% 
-  unite("value", mother_tongue, population, sep = "/") %>% 
-  pivot_wider(names_from = region, values_from = c(value)) %>% 
-  select(category, language, Toronto, Montréal, Vancouver, Calgary, Edmonton)
-
-
-
-
 # making a narrow data set 
 top5_cities_narrow <- top5_cities %>% 
+  select(region, category, language, most_at_home, most_at_work) %>% 
   pivot_longer(!c(region, category, language), names_to = "type", values_to = "count")
-
-write.csv(top5_cities_narrow, "region_lang_top5_cities_narrow.csv", row.names = F)
+write.csv(top5_cities_narrow, "region_lang_top5_cities_long.csv", row.names = F)
