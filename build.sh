a6e776cc2c163a254b96ed404372a64ce6c9e79c
Trevor Campbell <trevor.d.campbell@gmail.com>
2021-05-29 15:25:22 -0700

    increased docker container memory for book compile to avoid out of resoruces error

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-docker run --rm -v $PWD:/ids ubcdsci/intro-to-ds /bin/bash -c "cd /ids; Rscript build.R"
+docker run --rm -m 1g -v $PWD:/ids ubcdsci/intro-to-ds /bin/bash -c "cd /ids; Rscript build.R"
