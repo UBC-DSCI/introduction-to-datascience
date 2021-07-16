d0614d2f74b6091cc2cf51bdb9ba2876cb34b4d6
Trevor Campbell <trevor.d.campbell@gmail.com>
2021-07-16 14:34:11 -0700

    renamed build.R to _build.R to avoid autocomplete confusion with build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-docker run --rm -m 1g -v $PWD:/ids ubcdsci/intro-to-ds /bin/bash -c "cd /ids; Rscript build.R"
+docker run --rm -m 1g -v $PWD:/ids ubcdsci/intro-to-ds /bin/bash -c "cd /ids; Rscript _build.R"
