46a1146c9a9b32db8eb83bb04608ddca62a1d736
Trevor Campbell <trevor.d.campbell@gmail.com>
2021-07-21 16:25:53 -0700

    update dockerfile to v12

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-docker run --rm -m 4g -v $PWD:/ids ubcdsci/intro-to-ds:v0.11.0 /bin/bash -c "cd /ids; Rscript _build.R"
+docker run --rm -m 4g -v $PWD:/ids ubcdsci/intro-to-ds:v0.12.0 /bin/bash -c "cd /ids; Rscript _build.R"
