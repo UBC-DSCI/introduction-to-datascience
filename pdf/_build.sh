78cf3ca06245444ef7071db64c08f7a1362751d5
Tiffany Timbers <tiffany.timbers@gmail.com>
2021-10-04 16:44:34 -0700

    added all files to pdf/_build.sh and created a cleanup step after

--- pdf/_build.sh
+++ pdf/_build.sh
@@ -5,6 +5,16 @@ rm preface-text.Rmd \
   intro.Rmd \
   reading.Rmd \
   wrangling.Rmd \
+  viz.Rmd \
+  classification1.Rmd \
+  classification2.Rmd \
+  regression1.Rmd \
+  regression2.Rmd \
+  clustering.Rmd \
+  inference.Rmd \
+  jupyter.Rmd \
+  version-control.Rmd \
+  setup.Rmd .
   references.Rmd \
   printindex.tex
 rm -rf data img
@@ -15,6 +25,16 @@ cp ../preface-text.Rmd .
 cp ../intro.Rmd .
 cp ../reading.Rmd .
 cp ../wrangling.Rmd .
+cp ../viz.Rmd .
+cp ../classification1.Rmd .
+cp ../classification2.Rmd .
+cp ../regression1.Rmd .
+cp ../regression2.Rmd .
+cp ../clustering.Rmd .
+cp ../inference.Rmd .
+cp ../jupyter.Rmd .
+cp ../version-control.Rmd .
+cp ../setup.Rmd .
 cp ../references.Rmd .
 cp ../printindex.tex .
 cp -r ../data .
@@ -22,3 +42,22 @@ cp -r ../img .
 
 # Build the book with bookdown
 Rscript _build.r
+
+# clean Rmd files in pdf dir
+rm preface-text.Rmd \
+  intro.Rmd \
+  reading.Rmd \
+  wrangling.Rmd \
+  viz.Rmd \
+  classification1.Rmd \
+  classification2.Rmd \
+  regression1.Rmd \
+  regression2.Rmd \
+  clustering.Rmd \
+  inference.Rmd \
+  jupyter.Rmd \
+  version-control.Rmd \
+  setup.Rmd .
+  references.Rmd \
+  printindex.tex
+rm -rf data img
