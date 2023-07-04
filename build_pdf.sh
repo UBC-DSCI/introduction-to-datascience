25520b7e1168b8221d025d3d1ecbc8fe4d943116
Trevor Campbell <trevor.d.campbell@gmail.com>
2023-07-03 17:52:38 -0700

    build html working now (but main page is welcome.html...)

--- build_pdf.sh
+++ build_pdf.sh
@@ -1,6 +1,6 @@
 # Script to generate PDF book
 
 # Build the book with bookdown
-docker run --rm -m 5g -v $(pwd):/home/rstudio/introduction-to-datascience ubcdsci/intro-to-ds:v0.23.0 /bin/bash -c "cd /home/rstudio/introduction-to-datascience; Rscript -e 'bookdown::render_book(".", "bookdown::pdf_book")'"
+docker run --rm -m 5g -v $(pwd):/home/rstudio/introduction-to-datascience ubcdsci/intro-to-ds:v0.23.0 /bin/bash -c "cd /home/rstudio/introduction-to-datascience; Rscript -e bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
 
 
